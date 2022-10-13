d6e9bd5704f1c20357a4cec23c1f2d23effb10b8
MilenaViera <milenajohanna1@gmail.com>
2022-10-13 17:52:32 +0200

    chore: add model course and migration

---
+++ db/migrate/20221013153542_create_courses.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CreateCourses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.timestamps
+    end
+  end
+end
