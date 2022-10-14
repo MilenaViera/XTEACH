b97e037ed3499cc3e11f15ae33c0cc3baa0e35b3
MilenaViera <milenajohanna1@gmail.com>
2022-10-14 17:45:12 +0200

    feat: controller users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    GenerateReportNotesAverageJob.perform_now()
+    GenerateReportProgressAverageJob.perform_now()
+    @students_completed = GenerateReportStudentsCompletedJob.perform_now()
+    @students_graduated_per_inscription = GenerateReportStudentsGraduatedPerInscriptionJob.perform_now()
+    @students_no_graduated_per_inscription = GenerateReportStudentsNoGraduatedPerInscription.perform_now()
+  end
+end
